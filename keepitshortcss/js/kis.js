60d951cdf19e665806d029ca47c83b6e1d336921
Dasari Munikumar <128296321+Munikumar-Dasari@users.noreply.github.com>
2024-01-30 18:05:14 +0530

    Update kis.js

--- keepitshortcss/js/kis.js
+++ keepitshortcss/js/kis.js
@@ -5,6 +5,13 @@ const question4El = document.getElementById("question4");
 const question5El = document.getElementById("question5");
 const question6El = document.getElementById("question6");
 const question7El = document.getElementById("question7");
+const question8El = document.getElementById("question8");
+const question9El = document.getElementById("question9");
+const question10El = document.getElementById("question10");
+const question11El = document.getElementById("question11");
+const question12El = document.getElementById("question12");
+
+
 
 const answere1El =  document.getElementById("answere1");
 const answere2El =  document.getElementById("answere2");
@@ -13,6 +20,13 @@ const answere4El =  document.getElementById("answere4");
 const answere5El =  document.getElementById("answere5");
 const answere6El =  document.getElementById("answere6");
 const answere7El =  document.getElementById("answere7");
+const answere8El =  document.getElementById("answere8");
+const answere9El =  document.getElementById("answere9");
+const answere10El =  document.getElementById("answere10");
+const answere11El =  document.getElementById("answere11");
+const answere12El =  document.getElementById("answere12");
+
+let qaeNameEl = document.getElementById("qae-name");
 
 
 
@@ -24,6 +38,11 @@ function functionQae1(){
     answere5El.style.display = "none";
     answere6El.style.display = "none";
     answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.add("questions-active");
     question2El.classList.remove("questions-active");
@@ -32,6 +51,13 @@ function functionQae1(){
     question5El.classList.remove("questions-active");
     question6El.classList.remove("questions-active");
     question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question1El.textContent;
 
 }
 
@@ -43,6 +69,11 @@ function functionQae2(){
     answere5El.style.display = "none";
     answere6El.style.display = "none";
     answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.remove("questions-active");
     question2El.classList.add("questions-active");
@@ -51,6 +82,13 @@ function functionQae2(){
     question5El.classList.remove("questions-active");
     question6El.classList.remove("questions-active");
     question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question2El.textContent;
 
 }
 
@@ -62,6 +100,11 @@ function functionQae3(){
     answere5El.style.display = "none";
     answere6El.style.display = "none";
     answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.remove("questions-active");
     question2El.classList.remove("questions-active");
@@ -70,6 +113,13 @@ function functionQae3(){
     question5El.classList.remove("questions-active");
     question6El.classList.remove("questions-active");
     question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question3El.textContent;
 
 }
 
@@ -81,6 +131,11 @@ function functionQae4(){
     answere5El.style.display = "none";
     answere6El.style.display = "none";
     answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.remove("questions-active");
     question2El.classList.remove("questions-active");
@@ -89,6 +144,13 @@ function functionQae4(){
     question5El.classList.remove("questions-active");
     question6El.classList.remove("questions-active");
     question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question4El.textContent;
 
 }
 
@@ -100,6 +162,11 @@ function functionQae5(){
     answere5El.style.display = "block";
     answere6El.style.display = "none";
     answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.remove("questions-active");
     question2El.classList.remove("questions-active");
@@ -108,6 +175,13 @@ function functionQae5(){
     question5El.classList.add("questions-active");
     question6El.classList.remove("questions-active");
     question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question5El.textContent;
 
 }
 
@@ -119,6 +193,11 @@ function functionQae6(){
     answere5El.style.display = "none";
     answere6El.style.display = "block";
     answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.remove("questions-active");
     question2El.classList.remove("questions-active");
@@ -127,9 +206,15 @@ function functionQae6(){
     question5El.classList.remove("questions-active");
     question6El.classList.add("questions-active");
     question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question6El.textContent;
 
 }
-
 
 function functionQae7(){
     answere1El.style.display = "none";
@@ -139,6 +224,11 @@ function functionQae7(){
     answere5El.style.display = "none";
     answere6El.style.display = "none";
     answere7El.style.display = "block";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
 
     question1El.classList.remove("questions-active");
     question2El.classList.remove("questions-active");
@@ -147,6 +237,169 @@ function functionQae7(){
     question5El.classList.remove("questions-active");
     question6El.classList.remove("questions-active");
     question7El.classList.add("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question7El.textContent;
+
+}
+
+function functionQae8(){
+    answere1El.style.display = "none";
+    answere2El.style.display = "none";
+    answere3El.style.display = "none";
+    answere4El.style.display = "none";
+    answere5El.style.display = "none";
+    answere6El.style.display = "none";
+    answere7El.style.display = "none";
+    answere8El.style.display = "block";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
+
+    question1El.classList.remove("questions-active");
+    question2El.classList.remove("questions-active");
+    question3El.classList.remove("questions-active");
+    question4El.classList.remove("questions-active");
+    question5El.classList.remove("questions-active");
+    question6El.classList.remove("questions-active");
+    question7El.classList.remove("questions-active");
+    question8El.classList.add("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question8El.textContent;
+
+}
+
+function functionQae9(){
+    answere1El.style.display = "none";
+    answere2El.style.display = "none";
+    answere3El.style.display = "none";
+    answere4El.style.display = "none";
+    answere5El.style.display = "none";
+    answere6El.style.display = "none";
+    answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "block";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
+
+    question1El.classList.remove("questions-active");
+    question2El.classList.remove("questions-active");
+    question3El.classList.remove("questions-active");
+    question4El.classList.remove("questions-active");
+    question5El.classList.remove("questions-active");
+    question6El.classList.remove("questions-active");
+    question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.add("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+
+    qaeNameEl.textContent = question9El.textContent;
+
+}
+
+function functionQae10(){
+    answere1El.style.display = "none";
+    answere2El.style.display = "none";
+    answere3El.style.display = "none";
+    answere4El.style.display = "none";
+    answere5El.style.display = "none";
+    answere6El.style.display = "none";
+    answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "block";
+    answere11El.style.display = "none";
+    answere12El.style.display = "none";
+
+    question1El.classList.remove("questions-active");
+    question2El.classList.remove("questions-active");
+    question3El.classList.remove("questions-active");
+    question4El.classList.remove("questions-active");
+    question5El.classList.remove("questions-active");
+    question6El.classList.remove("questions-active");
+    question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.add("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question10El.textContent;
+
+}
+
+function functionQae11(){
+    answere1El.style.display = "none";
+    answere2El.style.display = "none";
+    answere3El.style.display = "none";
+    answere4El.style.display = "none";
+    answere5El.style.display = "none";
+    answere6El.style.display = "none";
+    answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "block";
+    answere12El.style.display = "none";
+
+    question1El.classList.remove("questions-active");
+    question2El.classList.remove("questions-active");
+    question3El.classList.remove("questions-active");
+    question4El.classList.remove("questions-active");
+    question5El.classList.remove("questions-active");
+    question6El.classList.remove("questions-active");
+    question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.add("questions-active");
+    question12El.classList.remove("questions-active");
+
+    qaeNameEl.textContent = question11El.textContent;
+
+}
+
+function functionQae12(){
+    answere1El.style.display = "none";
+    answere2El.style.display = "none";
+    answere3El.style.display = "none";
+    answere4El.style.display = "none";
+    answere5El.style.display = "none";
+    answere6El.style.display = "none";
+    answere7El.style.display = "none";
+    answere8El.style.display = "none";
+    answere9El.style.display = "none";
+    answere10El.style.display = "none";
+    answere11El.style.display = "none";
+    answere12El.style.display = "block";
+
+    question1El.classList.remove("questions-active");
+    question2El.classList.remove("questions-active");
+    question3El.classList.remove("questions-active");
+    question4El.classList.remove("questions-active");
+    question5El.classList.remove("questions-active");
+    question6El.classList.remove("questions-active");
+    question7El.classList.remove("questions-active");
+    question8El.classList.remove("questions-active");
+    question9El.classList.remove("questions-active");
+    question10El.classList.remove("questions-active");
+    question11El.classList.remove("questions-active");
+    question12El.classList.add("questions-active");
+
+    qaeNameEl.textContent = question12El.textContent;
 
 }
 
@@ -158,3 +411,8 @@ question4El.addEventListener("click", functionQae4);
 question5El.addEventListener("click", functionQae5);
 question6El.addEventListener("click", functionQae6);
 question7El.addEventListener("click", functionQae7);
+question8El.addEventListener("click", functionQae8);
+question9El.addEventListener("click", functionQae9);
+question10El.addEventListener("click", functionQae10);
+question11El.addEventListener("click", functionQae11);
+question12El.addEventListener("click", functionQae12);
